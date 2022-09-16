51f3b75abf06f995d204d6aa081e3728a4e9fb3b
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-16 20:26:12 +0300

    Add sprite package
    Add skinView script
    Add sprite for shop

---
+++ Assets/Scripts/UI/Shop/UISkinView.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISkinView : MonoBehaviour
+{
+    [SerializeField] private Image _imageTemplate;
+    [SerializeField] private Button _button;
+    [SerializeField] private TMP_Text _textButton;
+
+
+}
