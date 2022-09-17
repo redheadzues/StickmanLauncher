8bbe8728f33f72d47c9d2d6baf80a6e1511dd29f
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-17 18:10:04 +0300

    Update UICustomizer.cs

--- Assets/Scripts/UI/Shop/UICustomizer.cs
+++ Assets/Scripts/UI/Shop/UICustomizer.cs
@@ -1,8 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UICustomizer : UISkinShop
 {
+    private void OnEnable()
+    {
+        if (_views != null)
+            for (int i = 0; i < _views.Count; i++)
+                _views[i].Equiped += OnEquiped;
+    }
 
+    private void OnEquiped()
+    {
+        throw new NotImplementedException();
+    }
 }
