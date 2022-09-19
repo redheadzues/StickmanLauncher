8f2f59c979e0ec9dc33c1393177db85869d1b1ef
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-19 16:03:28 +0300

    Add scripts UISkinView UISkin

---
+++ Assets/Scripts/UI/Shop/UISkin.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class UISkin : MonoBehaviour
+{
+    [SerializeField] private Sprite _spriteIcon;
+    [SerializeField] private int _price;
+    [SerializeField] private bool isBuyed = false;
+    [SerializeField] private bool isEquiped = false;
+
+    private static int _ids;
+
+    public Sprite Icon => _spriteIcon;
+    public bool IsBuyed => isBuyed;
+    public bool IsEquiped => isEquiped;
+
+    public int Price => _price;
+
+    public int Id = ++_ids;
+
+    public void Buy()
+    {
+        isBuyed = true;
+    }
+
+    public void Equip()
+    {
+        isEquiped = true;
+    }
+
+    public void Unequip()
+    {
+        isEquiped = false;
+    }
+}
---
+++ Assets/Scripts/UI/Shop/UISkjnView.cs
@@ -0,0 +1,85 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISkinView : MonoBehaviour
+{
+    [SerializeField] private Image _imageTemplate;
+    [SerializeField] private Button _buttonGetSkin;
+    [SerializeField] private TMP_Text _textButton;
+    [SerializeField] private Image _imageCoin;
+
+    private const string c_Equip = "Одеть";
+    private const string c_Equiped = "Одето";
+    private UISkin _skin;
+
+    public event Action<UISkinView> BuyButtonClicked;
+    public event Action<UISkinView> Equiped;
+
+    public UISkin Skin => _skin;
+
+    private void OnEnable()
+    {
+        _buttonGetSkin.onClick.AddListener(OnGetSkinButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _buttonGetSkin.onClick.RemoveListener(OnGetSkinButtonClick);
+    }
+
+    public void Initialize(UISkin skin)
+    {
+        _skin = skin;
+        _imageTemplate.sprite = skin.Icon;
+        DisplayButton();
+    }
+
+    public void DisplayButton()
+    {
+        if (_skin.IsBuyed == false)
+            _textButton.text = _skin.Price.ToString();
+        else
+        {
+            _imageCoin.gameObject.SetActive(false);
+
+            if (_skin.IsEquiped == false)
+            {
+                _textButton.text = c_Equip;
+                ChangeButtonColor(Color.white);
+            }
+            else
+            {
+                _textButton.text = c_Equiped;
+                ChangeButtonColor(Color.yellow);
+            }
+        }
+    }
+
+    private void ChangeButtonColor(Color color)
+    {
+        _buttonGetSkin.image.color = color;
+    }
+
+    private void OnGetSkinButtonClick()
+    {
+        if (_skin.IsBuyed == false)
+            TrySellSkin();
+        else
+        {
+            if (_skin.IsEquiped == false)
+                EquipSkin();
+        }
+    }
+
+    private void TrySellSkin()
+    {
+        BuyButtonClicked?.Invoke(this);
+    }
+
+    private void EquipSkin()
+    {
+        Equiped?.Invoke(this);
+    }
+}
