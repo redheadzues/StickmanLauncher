28659fd7019c402b40ac855d28689c065eb89061
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-19 18:54:43 +0300

    Add script UiOpenButtoncustomizer

---
+++ Assets/Scripts/UI/Shop/UIButtonOpenCustomizer.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class UIButtonOpenCustomizer : MonoBehaviour
+{
+    [SerializeField] private UICustomizer _customizer;
+
+    private Button _buttonOpenCustomizer;
+
+    private void OnValidate()
+    {
+        _customizer = FindObjectOfType<UICustomizer>();
+    }
+
+    private void Awake()
+    {
+        _buttonOpenCustomizer = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        _buttonOpenCustomizer.onClick.AddListener(OnButtonOpenCustomizerClick);
+        _customizer.gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        _buttonOpenCustomizer.onClick.RemoveListener(OnButtonOpenCustomizerClick);
+    }
+
+    private void OnButtonOpenCustomizerClick()
+    {
+        _customizer.gameObject.SetActive(true);
+    }
+}
