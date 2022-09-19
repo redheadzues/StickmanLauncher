07252ebbb9668343037f0c91fe78bb27d58e5089
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-19 16:40:15 +0300

    Added scripts UICustomizer

---
+++ Assets/Scripts/UI/Shop/UICustomizer.cs
@@ -0,0 +1,26 @@
+
+public class UICustomizer : UISkinShop
+{
+    private void OnEnable()
+    {
+        for (int i = 0; i < _views.Count; i++)
+            _views[i].Equiped += OnEquiped;
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < _views.Count; i++)
+            _views[i].Equiped -= OnEquiped;
+    }
+
+    private void OnEquiped(UISkinView view)
+    {
+        for (int i = 0; i < _views.Count; i++)
+            _views[i].Skin.Unequip();
+
+        view.Skin.Equip();
+
+        for (int i = 0; i < _views.Count; i++)
+            _views[i].DisplayButton();
+    }
+}
