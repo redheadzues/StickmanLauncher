bd925fb6ae3c603920fcdd0805925b5ca0ceedbc
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-19 17:42:53 +0300

    Changed allied spawner

--- Assets/Scripts/Spawner/AlliedSpawner.cs
+++ Assets/Scripts/Spawner/AlliedSpawner.cs
@@ -1,15 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 public class AlliedSpawner : ObjectsPool
 {
-    [SerializeField] private GameObject _template;
+    [SerializeField] private List<UISkin> _templates;
     [SerializeField] private StickmanLauncher _launcher;
     [SerializeField] private Transform _startMovePosition;
+
+    private GameObject _template;
 
     public event UnityAction<GameObject> Instantiated;
 
     private void Awake()
     {
+        ChooseTemplate();
         InitializePool(_template);
     }
 
@@ -26,6 +30,16 @@ public class AlliedSpawner : ObjectsPool
     private void OnDisable()
     {
         _launcher.Successfully -= OnLaunched;
+    }
+
+    private void ChooseTemplate()
+    {
+        for(int i = 0; i < _templates.Count; i++)
+            if (_templates[i].IsEquiped == true)
+            {
+                _template = _templates[i].gameObject;
+                return;
+            }
     }
 
     private void Spawn()
