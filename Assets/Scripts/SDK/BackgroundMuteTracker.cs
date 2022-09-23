5ca1aab0258dba0e307647f58d048d7794546642
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-24 00:42:38 +0300

    add script backgroundmutetracker

---
+++ Assets/Scripts/SDK/BackgroundMuteTracker.cs
@@ -0,0 +1,37 @@
+using Agava.WebUtility;
+using System;
+using UnityEngine;
+
+public class BackgroundMuteTracker : MonoBehaviour
+{
+    [SerializeField] private SDKIntegration _sdkIntegration;
+
+    private void OnEnable()
+    {
+        _sdkIntegration.VideoOpened += OnVideoOpened;
+        _sdkIntegration.VideoClosed += OnVideoClosed;
+        WebApplication.InBackgroundChangeEvent += OnBackgroundChange;
+    }
+
+    private void OnDisable()
+    {
+        _sdkIntegration.VideoOpened -= OnVideoOpened;
+        _sdkIntegration.VideoClosed -= OnVideoClosed;
+        WebApplication.InBackgroundChangeEvent -= OnBackgroundChange;
+    }
+
+    private void OnVideoOpened()
+    {
+        OnBackgroundChange(true);
+    }
+
+    private void OnVideoClosed()
+    {
+        OnBackgroundChange(false);
+    }
+
+    private void OnBackgroundChange(bool isBackground)
+    {
+        AudioListener.pause = isBackground;
+    }
+}
