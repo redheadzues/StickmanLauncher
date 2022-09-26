35282842a35246812f2ff115b713c59717fe1a11
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-27 00:05:43 +0300

    Update UIButtonSound.cs

--- Assets/Scripts/UI/UIButtonSound.cs
+++ Assets/Scripts/UI/UIButtonSound.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class UIButtonSound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Sprite _spriteSoundOff;
+    [SerializeField] private Sprite _spriteSoundOn;
+
+    private Button _buttonSound;
+    private bool _isSoundPlay = true;
+
+    private void Awake()
     {
-        
+        _buttonSound = GetComponent<Button>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        _buttonSound.onClick.AddListener(OnButtonSoundClick);
+    }
+
+    private void OnDisable()
+    {
+        _buttonSound.onClick.RemoveListener(OnButtonSoundClick);
+    }
+
+    private void OnButtonSoundClick()
+    {
+        if(_isSoundPlay == true)
+        {
+            AudioListener.volume = 0;
+            _buttonSound.image.sprite = _spriteSoundOff;
+            _isSoundPlay = false;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            _buttonSound.image.sprite = _spriteSoundOn;
+            _isSoundPlay = true;
+        }
     }
 }
