9a58ae75f84d9b223abb0967be6980a4e0e34aed
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-29 09:09:55 +0300

    lean localization changed from Russia to ru

--- Assets/Scripts/LanguageSetter.cs
+++ Assets/Scripts/LanguageSetter.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using Lean.Localization;
 using Agava.YandexGames;
+using System.Collections;
 
+[RequireComponent(typeof(LeanLocalization))]
 public class LanguageSetter : MonoBehaviour
 {
-    [SerializeField] private LeanLocalization _localization;
+    private LeanLocalization _localization;
 
-    private void Start()
+    private void Awake()
     {
+        _localization = GetComponent<LeanLocalization>();
+    }
+
+    private IEnumerator Start()
+    {
+        while(YandexGamesSdk.IsInitialized != true)
+        {
+            yield return new WaitForSeconds(0.2f);
+        }
+
         _localization.SetCurrentLanguage(YandexGamesSdk.Environment.i18n.lang);
     }
+
 }
--- Assets/Scripts/SDK/SDKIntegration.cs
+++ Assets/Scripts/SDK/SDKIntegration.cs
@@ -7,7 +7,7 @@ using System;
 
 public class SDKIntegration : MonoBehaviour
 {
-   
+    public event Action Initialized;
     public event Action Rewarded;
     public event Action VideoOpened;
     public event Action VideoClosed;
@@ -55,6 +55,11 @@ public class SDKIntegration : MonoBehaviour
 #if YANDEX_GAMES
         Agava.YandexGames.VideoAd.Show(OnVideoOpenCallback, OnRewardedCallback, OnVideoCloseCallback, OnVideoErrorCallback);
 #endif
+    }
+
+    private void OnInitialized()
+    {
+        Initialized?.Invoke();
     }
 
     private void OnAdOpened()
