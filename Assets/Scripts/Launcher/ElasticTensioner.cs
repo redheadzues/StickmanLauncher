c52f0206742ea607270870ad476aa5c1a97ee6b6
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-30 14:14:06 +0300

    Change Tap on all screen

--- Assets/Scripts/Launcher/ElasticTensioner.cs
+++ Assets/Scripts/Launcher/ElasticTensioner.cs
@@ -9,8 +9,11 @@ public class ElasticTensioner : MonoBehaviour
     private float _minPointZ = -27;
     private float _maxPointZ = -20;
 
-
     private Rigidbody _rigidbody;
+    private Vector3 _startDragPoint;
+    private float _distance;
+    private Vector3 _offset;
+    private Vector3 _basePosition;
 
     public event UnityAction DragStarted;
     public event UnityAction DragFinished;
@@ -18,21 +21,31 @@ public class ElasticTensioner : MonoBehaviour
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _basePosition = transform.position;
     }
 
     private void OnMouseDown()
     {
         _rigidbody.isKinematic = true;
         transform.rotation = Quaternion.Euler(Vector3.zero);
+
+        _startDragPoint = GetRayPoint();
         DragStarted?.Invoke();
     }
 
     private void OnMouseDrag()
     {
-        var distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        _offset = GetRayPoint() - _startDragPoint;
+        transform.position = ClampPosition(_basePosition + _offset);
+    }
+
+    private Vector3 GetRayPoint()
+    {
+        _distance = Vector3.Distance(transform.position, Camera.main.transform.position);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Vector3 rayPoint = ray.GetPoint(distance);
-        transform.position = ClampPosition(rayPoint);
+        Vector3 rayPoint = ray.GetPoint(_distance);
+
+        return rayPoint;
     }
 
     private Vector3 ClampPosition(Vector3 point)
