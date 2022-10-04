d82dee1b54c1b977b4156236cfd485d2c91bbae7
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-10-04 21:27:55 +0300

    Create EnemyDifficulty script

--- Assets/Scripts/Enemy/EnemyDifficulty.cs
+++ Assets/Scripts/Enemy/EnemyDifficulty.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyDifficulty : MonoBehaviour
+public static class EnemyDifficulty
 {
-    // Start is called before the first frame update
-    void Start()
+    private static float _damage = SaveProgress.EnemyDamage;
+    private static int _castleHp = SaveProgress.EnemyCastleHp;
+    private static int _spawnCount = SaveProgress.EnemySpawnCount;
+
+    public static void IncreaseDamage(float value)
     {
-        
+        if(value > 0)
+        {
+            _damage += value;
+            SaveProgress.EnemyDamage = value;
+        }        
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void DecreaseDamage(float value)
     {
-        
+        if (value > 0)
+        {
+            _damage -= value;
+            SaveProgress.EnemyDamage = value;
+        }
+    }
+
+    public static void IncreaseCastleHp(int value)
+    {
+        if (value > 0)
+        {
+            _castleHp += value;
+            SaveProgress.EnemyCastleHp = value;
+        }
+    }
+
+    public static void DecreaseCastleHp(int value)
+    {
+        if (value > 0)
+        {
+            _castleHp -= value;
+            SaveProgress.EnemyCastleHp = value;
+        }
+    }
+
+    public static void IncreaseSpawnCount(int value)
+    {
+        if (value > 0)
+        {
+            _spawnCount += value;
+            SaveProgress.EnemySpawnCount = value;
+        }
+    }
+
+    public static void DecreaseSpawnCount(int value)
+    {
+        if (value > 0)
+        {
+            _spawnCount -= value;
+            SaveProgress.EnemySpawnCount = value;
+        }
     }
 }
